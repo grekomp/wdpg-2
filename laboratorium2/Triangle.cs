836a73c6e1abd72c331668aa14c6ba114e01c0d3
Student <Student@105-01>
2016-11-14 09:30:38 +0100

    Add project files.

---
+++ laboratorium2/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorium2
+{
+    class Triangle<T> where T : struct
+    {
+        public T a { get; protected set; }
+        public T b { get; protected set; }
+        public T c { get; protected set; }
+
+        public double alpha { get; protected set; }
+        public double beta { get; protected set; }
+        public double gamma { get; protected set; }
+
+        public Triangle(T a, T b, T c)
+        {
+
+        }
+
+    }
+}
